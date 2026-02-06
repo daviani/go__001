92857171c58eb15ee58d1282664df17419526ae2
Daviani Fillatre <dev@daviani.dev>
2026-02-06 18:27:19 +0100

    learn(structs): add User struct with Greet method

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/daviani/go__001/internal/calculator"
+	"github.com/daviani/go__001/internal/user"
 )
 
 func main() {
@@ -13,10 +14,12 @@ func main() {
 	name = "roi du go"
 	age = 1
 
+	myUser := user.User{Name: "me", Age: 12}
 	result, isNegative := calculator.Add(5, 3)
+
 	names := "roi du gooo"
 	ages := "2"
-	fmt.Println("Hello World", name, age, names, ages, result, isNegative)
+	fmt.Println("Hello World", name, age, names, ages, result, isNegative, myUser.Greet())
 	fmt.Printf("Type de ages: %T, valeur: %v\n", ages, ages)
 	fmt.Printf("Type de age: %T, valeur: %v\n", age, age)
 
