7cfaf36015f4f4e80275ce97acaae3d0398e7c8b
Daviani Fillatre <dev@daviani.dev>
2026-02-06 18:11:04 +0100

    learn(packages): extract Add function to internal/calculator package

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/daviani/go__001/internal/calculator"
 )
 
 func main() {
@@ -12,19 +13,11 @@ func main() {
 	name = "roi du go"
 	age = 1
 
-	result, isNegative := add(5, 3)
+	result, isNegative := calculator.Add(5, 3)
 	names := "roi du gooo"
 	ages := "2"
 	fmt.Println("Hello World", name, age, names, ages, result, isNegative)
 	fmt.Printf("Type de ages: %T, valeur: %v\n", ages, ages)
 	fmt.Printf("Type de age: %T, valeur: %v\n", age, age)
 
-}
-
-func add(a, b int) (int, error) {
-	result := a + b
-	if result < 0 {
-		return 0, errors.New("retour négatif")
-	}
-	return result, nil
 }
